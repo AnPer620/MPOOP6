ed9c25faa2fa0885c65ab929a38f3615df56e1cd
Angel292702 <>
2021-10-22 19:14:20 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+/**
+ * @author Angel Pérez
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("La suma es: "+suma(2,2));
+    }
+
+    /**
+     * 
+     * @param a  Primer entero de la ecuación
+     * @param b  Segundo entero de la suma
+     * @return   La suma de los enteros a más b
+     */
+    public static int suma(int a, int b) {
+        return a+b;        
+    }
+}
